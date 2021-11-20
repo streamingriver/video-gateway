f303189f48d1ea6c5b95be6f568679c10d2f143d
Mindaugas Žvirblis <mz@9g.lt>
2021-11-20 22:38:04 +0200

    initial commit

---
+++ httphandlers.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"gitlab.com/avarf/getenvs"
+)
+
+type Response struct {
+	body    []byte
+	headers http.Header
+	code    int
+	err     error
+}
+
+type IFetcher interface {
+	Fetch(string) *Response
+}
+
+func NewFetcher() *Fetcher {
+	return &Fetcher{}
+}
+
+type Fetcher struct {
+}
+
+func (f Fetcher) Fetch(url string) *Response {
+
+	hc := http.Client{Timeout: 10 * time.Second}
+
+	request, err := http.NewRequest("GET", url, nil)
+	request.Header.Set("User-Agent", "streaminriveriptv/1.0")
+
+	response, err := hc.Do(request)
+	if err != nil {
+		return &Response{
+			err: err,
+		}
+	}
+
+	if response.StatusCode/100 != 2 {
+		return &Response{
+			err:  fmt.Errorf("Invalid status code: %v", response.StatusCode),
+			code: response.StatusCode,
+		}
+	}
+	defer response.Body.Close()
+	b, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return &Response{
+			err:  err,
+			code: response.StatusCode,
+		}
+	}
+	r := &Response{
+		body:    b,
+		headers: response.Header.Clone(),
+		code:    response.StatusCode,
+	}
+	// copy(r.body, b)
+	return r
+
+}
+
+type HTTPHandler struct {
+	registry *Registry
+	Fetcher  IFetcher
+	tokens   *Tokens
+}
+
+func (hh *HTTPHandler) ping(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	hh.registry.ping(vars["name"], vars["host"], vars["port"])
+}
+
+func (hh HTTPHandler) main(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	if !hh.tokens.Check(vars["token"], vars["name"], r) {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+
+	url := hh.registry.getURL(vars["name"], vars["file"])
+
+	if url == nil {
+		response := hh.Fetcher.Fetch(fmt.Sprintf("http://%s/%s/%s", getenvs.GetEnvString("FFMPEG_SERVER_HOST", "video-transcoding"), vars["name"], vars["file"]))
+
+		w.Header().Set("Content-Lenght", fmt.Sprintf("%d", len(response.body)))
+		w.Write(response.body)
+		return
+	}
+
+	response := hh.Fetcher.Fetch(*url)
+	if response.err != nil {
+		log.Printf("fetch url error: %v", response.err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	for k, v := range response.headers {
+		for _, vv := range v {
+			w.Header().Set(k, vv)
+		}
+	}
+	w.Header().Set("Content-Lenght", fmt.Sprintf("%d", len(response.body)))
+	w.Write(response.body)
+}
---
+++ tokens.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
+
+func NewTokens(url string, other ...interface{}) *Tokens {
+	t := &Tokens{
+		fetchFrom: url,
+		mu:        &sync.RWMutex{},
+		maps:      &T{make(map[string]bool), make(map[string]string), make(map[string]string)},
+		work:      false,
+	}
+	return t
+}
+
+type Tokens struct {
+	fetchFrom string
+	mu        *sync.RWMutex
+	maps      *T
+	work      bool
+}
+
+type T struct {
+	ADDR map[string]bool   `json:"addr"`
+	IP   map[string]string `json:"ip"`
+	CH   map[string]string `json:"ch"`
+}
+
+// Check if user match conditions and can watch
+func (t Tokens) Check(token, channel string, req *http.Request) bool {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
+	host := req.Header.Get("X-Real-IP")
+
+	ip, ok := t.maps.IP[token]
+	if !ok {
+		return false
+	}
+	if ip != host {
+		return false
+	}
+	ch, ok := t.maps.CH[token]
+	if !ok {
+		return false
+	}
+	if ch != channel {
+		return false
+	}
+
+	return true
+}
+
+// Worker periodicaly fetches tokens from api
+func (t *Tokens) Worker() {
+	t.work = true
+	go t.updateTokens()
+}
+
+func (t *Tokens) StopWorker() {
+	t.work = false
+}
+
+func (t *Tokens) updateTokens() {
+	for {
+		if !t.work {
+			break
+		}
+		t.tokens()
+		time.Sleep(2 * time.Second)
+
+	}
+}
+
+func (t *Tokens) tokens() {
+	body := t.remoteGet(t.fetchFrom)
+	if body == nil {
+		return
+	}
+	t.mu.Lock()
+	err := json.Unmarshal(body, t.maps)
+	t.mu.Unlock()
+	if err != nil {
+		log.Printf("updateTokens error: %v", err)
+		return
+	}
+}
+
+func (t *Tokens) remoteGet(url string) []byte {
+	response, err := http.Get(url)
+	if err != nil {
+		log.Printf("remote_get error: %v", err)
+		return nil
+	}
+	defer response.Body.Close()
+	b, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("remote_get error: %v", err)
+		return nil
+	}
+
+	return b
+}
